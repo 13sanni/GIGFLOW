cce16974a69891a4ff210cb101cfd346222f7b5a
ss752 <ss7527767@gmail.com>
2026-01-11 20:43:21 +0530

    created user model

---
+++ backend/src/models/user.model.ts
@@ -0,0 +1,18 @@
+import mongoose, {Document,Schema } from "mongoose";
+
+export interface User extends Document{
+    name:string,
+    email:string,
+    password:string,
+    
+}
+
+const userSchema = new Schema<User>({
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+},
+{timestamps:true}
+)
+const User = mongoose.model<User>("User", userSchema);
+export default User
