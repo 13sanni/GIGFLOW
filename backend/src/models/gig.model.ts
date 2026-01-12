572d6dcf316696d29e440da4a2e746e20bd4744c
ss752 <ss7527767@gmail.com>
2026-01-12 22:18:19 +0530

    made gig model

---
+++ backend/src/models/gig.model.ts
@@ -0,0 +1,23 @@
+import mongoose, { Document, Schema } from "mongoose";
+export interface Gig extends Document {
+    title: string,
+    description: string,
+    budget: number,
+    status: string,
+    owner: mongoose.Schema.Types.ObjectId,
+
+}
+const gigSchema = new Schema<gig>({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    budget: { type: Number, required: true },
+    status: { type: String, required: true, default: "open" },
+    owner: {
+        type: Schema.Types.ObjectId, ref: "User", required: true
+    }
+},
+    { timestamps: true }
+);
+
+const Gig = mongoose.model<Gig>("Gig", gigSchema);
+export default Gig
