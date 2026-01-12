6033e0dd77cc95a883dbf80c980d48e9b1987eae
ss752 <ss7527767@gmail.com>
2026-01-12 22:00:41 +0530

    created a auth middleware to verify token and  attached it to routes

---
+++ backend/src/middlewares/auth.middleware.ts
@@ -0,0 +1,20 @@
+import { type Request,type Response,type NextFunction } from "express";
+
+import jwt from "jsonwebtoken";
+const authMiddleware = (req :Request, res:Response,next:NextFunction) => {
+let token = req.cookies['token'];
+if (!token) {
+    return res.status(401).json({ success: false, message: "Unauthorized" });}
+
+const secretKey = process.env.JWT_SECRET_KEY as string;
+     jwt.verify(token, secretKey, (err:any , decoded:any) => {
+          
+    if (err) {
+        return res.status(401).json({ success: false, message: "Unauthorized" });
+    } else {
+        (req as any).user = decoded;
+         next();
+    } })
+   
+}     
+export default authMiddleware;
--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,11 +1,14 @@
-import { Router } from "express";
+import { response, Router } from "express";
+import { type Request, type Response, type NextFunction } from "express";
 import { registerUser,loginUser} from "../controllers/auth.controller.js";
+import authMiddleware from "../middlewares/auth.middleware.js";
 const router = Router();
 
 router.post("/register",registerUser)
 router.post("/login",loginUser);
-
-
+router.get("/test",authMiddleware,(req:Request,res:Response)=>{
+    res.status(200).json({message:"Auth route working fine",user:(req as any).user})
+    });
 
 
 
