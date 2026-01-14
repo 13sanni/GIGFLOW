515b0069074e997edc4c6d5f1026f7eb20b97ccb
ss752 <ss7527767@gmail.com>
2026-01-14 15:27:46 +0530

    .

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import store from "./store/Index.jsX";
+import store from "./store/Index.jsx";
 import App from "./App";
 import "./index.css";
 import { Provider } from "react-redux";
