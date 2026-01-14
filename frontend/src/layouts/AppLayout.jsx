60d028d07e2479d06c2931e8a66faecc2608c32f
ss752 <ss7527767@gmail.com>
2026-01-14 05:43:21 +0530

    add socket.io frontend listeners for bid events

--- frontend/src/layouts/AppLayout.jsx
+++ frontend/src/layouts/AppLayout.jsx
@@ -1,7 +1,33 @@
 import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
 import Navbar from "../components/Navbar";
+import socket from "../socket/Socket.jsx";
 
 const AppLayout = () => {
+  useEffect(() => {
+    socket.connect();
+
+    socket.on("connect", () => {
+      console.log("Socket connected:", socket.id);
+    });
+
+    socket.on("bid:new", (data) => {
+      console.log("New bid received:", data);
+      alert("New bid received on your gig!");
+    });
+
+    socket.on("bid:accepted", (data) => {
+      console.log("Bid accepted:", data);
+      alert("You have been hired!");
+    });
+
+    return () => {
+      socket.off("bid:new");
+      socket.off("bid:accepted");
+      socket.disconnect();
+    };
+  }, []);
+
   return (
     <>
       <Navbar />
